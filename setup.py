82cc3b8f48bba63b010eb704aea2fb69b07114a3
Rupesh Singh <wrufesh@gmail.com>
2021-05-14 11:40:02 +0545

    minor requirements update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='rhrs_plugin_builder',
-    version='0.3',    
+    version='0.4',    
     description='RealHRSoft payroll calculator plugin builder',
     url='https://github.com/aayulogic/payroll-plugin-builder.git',
     author='Rupesh Singh',
     author_email='rupesh.singh@aayulogic.com',
     install_requires=[
-        'Cython>=0.29.23',
+        'Cython>=0.29',
         'PGPy>=0.5.4'                     
     ],
     classifiers=[
